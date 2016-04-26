2e35306aff3158eb2f37d6aca945e22c476b19f0
Dillon Dixon <dillondixon@gmail.com>
2016-04-25 23:42:20 -0700

    Added documentation to driver.py file and setuptool data for server

--- server/lights_433/driver.py
+++ server/lights_433/driver.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 import codecs
 from collections import namedtuple
 import logging
 import serial
 import struct
-import time
 
 _PROTOCOL_HEADER = b'CLS'
 _PROTOCOL_VERSION = b'0'
@@ -25,6 +29,7 @@ _WRONG_VERSION = b'H'
 _HEARTBEAT = b'I'
 _UNKNOWN_OP_CODE = b'Z'
 
+# For debugging purposes
 _RESPONSE_DEFINITIONS = {
     _AWAITING_DATA: "awaiting additional data",
     _INCOMING_DATA: "there is an incoming radio transmission",
@@ -42,6 +47,11 @@ LOG = logging.getLogger(__name__)
 
 
 class BadResponseError(Exception):
+    """
+    Represents an either unintelligable or unexpected response from the
+    serial device.
+    """
+
     def __init__(self, actual, expected):
         actual_msg = _RESPONSE_DEFINITIONS \
                      .get(actual, "Strange response: %s" % repr(actual))
@@ -52,21 +62,47 @@ class BadResponseError(Exception):
 
 
 class RadioTimeout(Exception):
+    """
+    Raised in the event of a radio timeout when waiting for an incoming signal.
+    """
     pass
 
 
 class SignalDriver(object):
 
     def __init__(self, serial_device_file, baud_rate=_DEFAULT_BAUD,
-                 timeout=_DEFAULT_TIMEOUT):
+                 timeout=_DEFAULT_TIMEOUT, port_setup=None):
+        """
+        Connects to a serial interface and initializes a new signal
+        driver instance.
 
+        :param serial_device_file: the time to resolve
+        :type serial_device_file: str or unicode
+        :param baud_rate: the baud rate of the interface
+        :type baud_rate: int
+        :param timeout: timeout of the interface in seconds
+        :type timeout: int
+        :param port_setup: a function to do any necessary work in restarting
+                           the interface or device (default None)
+        :type port_setup: function
+
+        :returns: tuple of days and datetime.time
+        """
         self.serial_device_file = serial_device_file
         self.baud_rate = baud_rate
         self.timeout = timeout
         self.conn = None
+        self.port_setup = port_setup
+        if self.port_setup is not None and \
+           not hasattr(self.port_setup, '__call__'):
+            raise Exception('Supplied argument for port_setup '
+                            'is not a function')
         self.reconnect()
 
     def reconnect(self):
+        """
+        Reconnects to the serial interface and resets the device (optional)
+        """
         if self.conn:
             try:
                 self.conn.close()
@@ -77,31 +113,71 @@ class SignalDriver(object):
                                   self.baud_rate,
                                   timeout=self.timeout)
 
-        # Time for the ATMega328P to complete boot in the event of a
-        # serial-triggered reset.
-        time.sleep(3)
+        if self.port_setup:
+            port_setup()
 
     def _assert_response(self, expected, actual=None):
+        """
+        Asserts that the next response matches the expected response and
+        raises an exception otherwise.
+
+        :param expected: the expected response
+        :type expected: byte
+        :param actual: the actual response (read from interface if unspecified)
+        :type actual: byte
+
+        :returns: none; raises error on assertion failure
+        """
         if not actual:
             actual = self.conn.read()
         if actual != expected:
             raise BadResponseError(actual, expected)
 
     def _read_2byte_int(self):
+        """
+        Reads a two-byte integer from the serial interface
+        (i.e. short on most desktop systems)
+
+        :returns: int
+        """
         return struct.unpack('<H', self.conn.read(2))
 
     def _write_as_2bytes(self, x):
+        """
+        Writes a number/character to the serial interface as 2-bytes
+        (i.e. short on most desktop systems)
+
+        :param x: the number to write
+        :type x: int or byte
+        """
         if isinstance(x, str):
             x = ord(x)
         self.conn.write(struct.pack("<H", x))
 
     def _perform_handshake(self):
+        """
+        Performs the handshake with the serial device to initializes
+        accepting incoming commands.
+        """
         self.conn.write(_PROTOCOL_HEADER)
         self.conn.write(_PROTOCOL_VERSION)
         self.conn.flush()
         self._assert_response(_HELLO)
 
     def send_signal(self, message, pulse_length, repetitions=1, protocol=1):
+        """
+        Instructs the serial device to broadcast a 433MHz signal with the
+        specified properties.
+
+        :param message: the message to send
+        :type message: str or unicode
+        :param pulse_length: pulse length of the message in microseconds
+        :type pulse_length: int
+        :param repetitions: number of times to broadcase the signal (default 1)
+        :type repetitions: int
+        :param protocol: the protocol version to use (default 1)
+        :type protocol: int
+        """
         self._perform_handshake()
         self.conn.write(_WRITE_433)
         self.conn.flush()
@@ -119,6 +195,17 @@ class SignalDriver(object):
         self._assert_response(_GOODBYE)
 
     def read_signals(self, message_num, radio_timeout=10000):
+        """
+        Reads a specified number of captured 433MHz broadcasts within a certain
+        timeout.
+
+        :param message_num: the number of messages to capture
+        :type message_num: int
+        :param radio_timeout: the number of milliseconds to wait before timeout
+        :type radio_timeout: int
+
+        :returns: generator of Signal
+        """
         self._perform_handshake()
         self.conn.write(_READ_433)
         self.conn.flush()
---
+++ server/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+from io import open
+
+setup(
+    name='lights-433',
+    packages=find_packages(exclude=["tests"]),
+    version=0.1,
+    description='A small webserver for talking to an Arduino that controls'
+                '433Mhz inbound and outbound radios.',
+    author='Dillon Dixon',
+    author_email='dillondixon@gmail.com',
+    url='https://github.com/ownaginatious/lights-433',
+    license='MIT',
+    zip_safe=True,
+    install_requires=[line.strip()
+                      for line in open("requirements.txt", "r",
+                                       encoding="utf-8").readlines()],
+)
