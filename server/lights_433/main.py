78dd8dcd0fc63fcc33c573b9a58d71f3bfecfb8e
Dillon Dixon <dillondixon@gmail.com>
2016-04-26 23:24:42 -0700

    Implemented the server component

--- server/lights_433/main.py
+++ server/lights_433/main.py
@@ -2,3 +2,38 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from .server import Lights433Server
+import clip
+
+app = clip.App()
+
+
+@app.main(description='An HTTP server daemon for controlling 433MHz '
+                      'light switches')
+@clip.opt('-h', '--host', default='127.0.0.1', type=str,
+          help='The interface to listen and permit connections on')
+@clip.opt('-p', '--port', default=8080, type=int,
+          help='The port to run the server on')
+@clip.opt('-g', '--gpio', required=False,
+          help='The device for controlling GPIO (if necessary)')
+@clip.arg('serial', required=True, help='The port to the serial device '
+                                        'that generates the signals')
+@clip.opt('-b', '--baud', required=False, default=9600, type=int,
+          help='Baud rate of the serial interface')
+@clip.opt('-t', '--timeout', required=False, default=1, type=int,
+          help='The timeout of the serial interface')
+@clip.arg('spec', required=True, help='Path to the spec file for users '
+                                      'and signals')
+def lights433(host, port, gpio, serial, baud, timeout, spec):
+    server = Lights433Server(host, port, gpio, serial, baud, timeout, spec)
+    server.start()
+
+
+def main():
+    try:
+        app.run()
+    except clip.ClipExit:
+        pass
+
+if __name__ == '__main__':
+    main()
--- server/lights_433/server.py
+++ server/lights_433/server.py
@@ -2,3 +2,102 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
+from flask import Flask, jsonify, make_response
+from flask_basic_roles import BasicRoleAuth
+import RPi.GPIO as GPIO
+import time
+from .driver import SignalDriver
+
+_RESET_PORT = 3
+
+
+class UnknownConfigSettingError(Exception):
+    pass
+
+
+class UserAlreadyExistsError(Exception):
+    pass
+
+
+class SwitchAlreadyExistsError(Exception):
+    pass
+
+
+class Lights433Server(object):
+
+    def __init__(self, host, port, gpio=None, serial, baud, timeout, spec):
+
+        if gpio:
+            # It is assumed this is a Raspberry Pi.
+            # We will setup port 3.
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(_RESET_PORT, GPIO.OUT)
+            GPIO.output(_RESET_PORT, GPIO.HIGH)
+
+            def _reset_port():
+                GPIO.output(_RESET_PORT, GPIO.LOW)
+                time.sleep(2)
+                GPIO.output(_RESET_PORT, GPIO.HIGH)
+
+            gpio = _reset_port
+
+        self.host = host
+        self.port = port
+        self.driver = SignalDriver(serial,
+                                   baud_rate=baud, timeout=timeout,
+                                   port_setup=gpio if gpio else lambda: None)
+        users = {}
+        switches = {}
+        with open(spec, 'r') as f:
+            for line in f:
+                if line.startsWith('switch:'):
+                    _, switch_id, on_signal, off_signal, pulse_length, \
+                        users = line.split(':')
+                    switches[switch_id] = dict(on_signal=on_signal,
+                                               off_signal=off_signal,
+                                               pulse_length=pulse_length,
+                                               users=users.split(','))
+                    raise SwitchAlreadyExistsError(switch_id)
+                elif line.startsWith('user:'):
+                    _, user_id, password = line.split(':')
+                    users[user_id] = password
+                    raise UserAlreadyExistsError(user_id)
+                else:
+                    raise UnknownConfigSetting(line.split(':')[0])
+
+        self.app = Flask(__name__)
+        auth = BasicRoleAuth()
+
+        for user_id, password in users:
+            auth.add_user(user=user_id, password=password)
+
+        for switch_id, conf in switches.items():
+
+            self.app.route('/switch/%s/<op>')
+            auth.require(users=conf.users)
+
+            def switch(op):
+                if op.lower() == 'on':
+                    self.driver.send_signal(conf.on_signal,
+                                            conf.pulse_length, 5)
+                    return make_response(
+                            jsonify(message='Switch \"%s\" on!' % switch_id),
+                            200)
+                elif op.lower() == 'off':
+                    self.driver.send_signal(conf.off_signal,
+                                            conf.pulse_length, 5)
+                    return make_response(
+                            jsonify(message='Switch \"%s\" off!' % switch_id),
+                            200)
+                else:
+                    return make_response(
+                               jsonify(error='no such switch \"%s\" or '
+                                             'method "%s"' % (switch_id, op)),
+                               404)
+
+    def run(self):
+        self.run()
+
+    app = Flask(__name__)
+    auth = BasicRoleAuth()
