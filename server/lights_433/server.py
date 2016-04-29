422208786f99eb06a3d8353054253eb49e3b451c
Dillon Dixon <dillondixon@gmail.com>
2016-04-29 00:13:29 -0700

    Changed function name to byte string rather than unicode

--- server/lights_433/server.py
+++ server/lights_433/server.py
@@ -109,7 +109,7 @@ class Lights433Server(object):
         for switch_id, conf in switches.items():
 
             switch_func = lambda op: switch(op, switch_id, conf)
-            switch_func.__name__ = switch_id
+            switch_func.__name__ = str(switch_id)
 
             self.app.route('/switch/%s/<op>' % switch_id)(
                 auth.require(users=conf['users'])(
